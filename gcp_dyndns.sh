5efe295621ed8cd0cd82df50d7e61fbfa11ad648
cesar b <cesar@pissedoffadmins.com>
2022-11-20 21:08:45 -0500

    changes: - added a usage / help section - removed redundant getopts options

--- gcp_dyndns.sh
+++ gcp_dyndns.sh
@@ -31,6 +31,8 @@ set -o pipefail
 set -u
 trap 'echo "${NAME}: Ouch! Quitting." 1>&2 ; exit 1' 1 2 3 9 15
 
+    readonly PROGNAME=$(basename $0)
+    readonly PROGIDR=$(readlink -m $(dirname $0))
 function main()
 {
     readonly RED=$(tput setaf 1)
@@ -92,8 +94,65 @@ function _CHANGE_IP()
     fi
 }
 
+function _USAGE()
+{
+    clear
+echo -e "
+NAME
+    ${PROGNAME}
+
+SYNOPSIS
+    ${PROGNAME} [OPTION]...
+
+DESCRIPTION
+    A script to enable automated updates to the A record
+    in Google Cloud DNS for usage similar to DYNDNS
+
+OPTIONS
+    -d [FQDN]
+            The DNS name that matches the incoming queries with this zone's
+            DNS name as its suffix.
+            This can be found in the second column by running :
+
+                    /path/to/gcloud dns managed-zones list
+
+    -g [path to gcloud]
+            The path to the gcloud command as installed by the
+            Google Cloud SDK installer.
+            Example: /home/user/google-cloud-sdk/bin/
+
+    -t [TTL]
+            The TTL in seconds that the resolver caches this resource
+            record set.
+            This defaults to 300 (5 minutes).
+
+    -r [resource type]
+            The resource record type of this resource record set.
+            This defaults to A.
+
+    -z [zone name]
+            The managed zone that this resource record set is affiliated with.
+            For example, my-zone-name; the name of this resource record set
+            must have the DNS name of the managed zone as its suffix.
+            This can be found in the first column by running :
+
+                    /path/to/gcloud dns managed-zones list
+
+Examples
+     Update the A record for FQDN FOOBAR.BAZ with zone name EX-SET:
+
+            ${PROGNAME} -g ~/google-cloud-sdk/bin -d foobar.baz -z EX-SET
+
+Requirement
+     This script requires that the Google Cloud SDK tools are installed and
+     configured and that the command gcloud is functioning against your
+     Google Cloud account with access to Google Coud DNS.
+
+    "
+}
+
 ## option selection
-while getopts "d:D:g:G:t:T:r:R:z:Z:" OPT
+while getopts "d:g:t:r:z:" OPT
 do
     case "${OPT}" in
         'd'|'D')
@@ -116,15 +175,12 @@ do
             ## Google cloud dns zone name.
             ZONE=${OPTARG}
             ;;
-        *)
-            echo "no options given"
-            exit 0
-            ;;
     esac
 done
 if [[ ${OPTIND} -eq 1 ]]
 then
-    echo "no options given"
+    _USAGE \
+        | less
     exit 0
 fi
 shift $((OPTIND-1))
